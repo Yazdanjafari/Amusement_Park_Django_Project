c2c354501d700c77a1173faaf64fc08f12f1fb9b
yazdan <yazdan10jr1@gmail.com>
2024-12-28 19:58:36 +0330

    fix 90% of front end

--- Amusement_Park_Application/urls.py
+++ Amusement_Park_Application/urls.py
@@ -5,5 +5,12 @@ app_name = "Amusement_Park"
 
 urlpatterns = [
     path("Products", views.products, name= "Products"),
+    path("Cart", views.cart, name= "Cart"),
+    path("Checkout", views.checkout, name= "Checkout"),
+    path("Empty-cart", views.empty_cart, name= "Empty-cart"),
+    path("Mobile_Error", views.mobile_error, name= "Mobile_Error"),
+    path("Refund", views.refund, name= "Refund"),
+    path("Setting", views.setting, name= "Setting"),
+    path("Submit_Pay", views.submit_pay, name= "Submit_Pay"),
 ]
 
--- Amusement_Park_Application/views.py
+++ Amusement_Park_Application/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
+
+# ---------------------------------------------   --------------------------------------------- #
 def products(request):
     return render(request, "Amusement_Park_Application/index.html")
+# ---------------------------------------------   --------------------------------------------- #
+def cart(request):
+    return render(request, "Amusement_Park_Application/Cart.html")
+# ---------------------------------------------   --------------------------------------------- #
+def checkout(request):
+    return render(request, "Amusement_Park_Application/Checkout.html")
+# ---------------------------------------------   --------------------------------------------- #
+def empty_cart(request):
+    return render(request, "Amusement_Park_Application/Empty-cart.html")
+# ---------------------------------------------   --------------------------------------------- #
+def mobile_error(request):
+    return render(request, "Amusement_Park_Application/Mobile_Error.html")
+# ---------------------------------------------   --------------------------------------------- #
+def refund(request):
+    return render(request, "Amusement_Park_Application/Refund.html")
+# ---------------------------------------------   --------------------------------------------- #
+def setting(request):
+    return render(request, "Amusement_Park_Application/Setting.html")
+# ---------------------------------------------   --------------------------------------------- #
+def submit_pay(request):
+    return render(request, "Amusement_Park_Application/Submit_Pay.html")
+
